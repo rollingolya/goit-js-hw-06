3b532c21b9e10801fe92dbd5c330483c9f52e745
rollingolya <107012291+rollingolya@users.noreply.github.com>
2022-10-24 02:53:53 +0300

    adds 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnRef = document.querySelector('.change-color');
+const spanRef = document.querySelector('.color');
+const body = document.querySelector('body');
+
+btnRef.addEventListener('click', onBtnChangeColor);
+
+function onBtnChangeColor(event) {
+const randomColor = getRandomHexColor();
+body.style.backgroundColor = randomColor;
+spanRef.textContent = randomColor;
+}
