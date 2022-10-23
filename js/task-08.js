241a6fff3f1fdff95f52fdf3c54a4962cf96e7f7
rollingolya <107012291+rollingolya@users.noreply.github.com>
2022-10-24 02:39:32 +0300

    adds 8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit (event) {
+event.preventDefault();
+
+const {
+    elements: {
+        email,
+        password
+    }
+} = event.currentTarget;
+
+if(email.value === "" || password.value === "") {
+alert('Всі поля повинні бути заповнені!');
+}
+else {
+    console.log({'email': email.value, 'password': password.value});
+    form.reset();
+}
+}
+
 
